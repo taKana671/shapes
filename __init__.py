a8671579f022b79ca43469729a01a48e49517520
taKana671 <kanae5321@gmail.com>
2025-11-30 01:43:16 +0900

    Added cubesphere.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
 from .cylinder import Cylinder
 from .plane import Plane
 from .box import Box
-from .sphere import Sphere, Icosphere
+from .sphere import Sphere
 from .torus import Torus
 from .cone import Cone
 from .right_triangular_prism import RightTriangularPrism
 from .elliptical_prism import EllipticalPrism
 from .capsule import Capsule
-from .rounded_box.capsule_prism import CapsulePrism
-from .rounded_box.rounded_corner_box import RoundedCornerBox
-from .rounded_box.rounded_edge_box import RoundedEdgeBox
+from .rounded_box import CapsulePrism, RoundedCornerBox, RoundedEdgeBox
 from .ellipsoid import Ellipsoid
+from .spherical_polyhedron import Icosphere, Cubesphere
---
+++ rounded_box/__init__.py
@@ -0,0 +1,3 @@
+from .capsule_prism import CapsulePrism
+from .rounded_corner_box import RoundedCornerBox
+from .rounded_edge_box import RoundedEdgeBox
